313f41635c59c6513032a17180e195fbddbd1b40
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-02-12 16:11:00 +0200

    Added Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,52 +9,71 @@ import { fadeIn, slideInLeft } from "@/utils/transitions";
 import PersonalInfo from "@/components/PersonalInfo";
 import "./style.css";
 import Skills from "@/components/Skills";
+import projects from "@/data/projects";
+import ProjectCard from "@/components/ProjectCard";
 
 export default function Home() {
-  return (
-    <div className="wrapper">
-      <Navbar />
-      <main className="content">
-        <motion.div {...fadeIn} className="section">
-          <div className="flex flex-col">
-            <Text size="xl" weight="bold" className="text-white">
-              Hi, I'm Eeka
-            </Text>
-            <Text size="md" className="text-gray-300" italic>
-              I'm a full stack developer
-            </Text>
+    return (
+        <div className="wrapper">
+            <Navbar />
+            <main className="content">
+                <motion.div {...fadeIn} className="section">
+                    <div className="flex flex-col">
+                        <Text size="xl" weight="bold" className="text-white">
+                            Hi, I'm Eeka
+                        </Text>
+                        <Text size="md" className="text-gray-300" italic>
+                            I'm a full stack developer
+                        </Text>
 
-            <motion.div {...slideInLeft} className="flex mt-10">
-              <PersonalInfo />
-            </motion.div>
-          </div>
-        </motion.div>
+                        <motion.div {...slideInLeft} className="flex mt-10">
+                            <PersonalInfo />
+                        </motion.div>
+                    </div>
+                </motion.div>
 
-        <motion.div {...fadeIn} transition={{ delay: 0.2 }} className="section">
-          <Skills />
-        </motion.div>
+                <motion.div
+                    {...fadeIn}
+                    transition={{ delay: 0.2 }}
+                    className="section"
+                >
+                    <Skills />
+                </motion.div>
 
-        <motion.div {...fadeIn} transition={{ delay: 0.2 }} className="section">
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <Text size="xl" weight="bold" className="text-white">
-                Some of my projects
-              </Text>
-              <Text
-                size="sm"
-                className="projects-description text-gray-300"
-                italic
-              >
-                Here are some of my projects that I've worked on or I'm
-                currently working on
-              </Text>
-            </div>
-            <Link href="/projects">
-              <Button>All Projects</Button>
-            </Link>
-          </div>
-        </motion.div>
-      </main>
-    </div>
-  );
+                <motion.div
+                    {...fadeIn}
+                    transition={{ delay: 0.2 }}
+                    className="section"
+                >
+                    <div className="flex justify-between">
+                        <div className="flex flex-col">
+                            <Text
+                                size="xl"
+                                weight="bold"
+                                className="text-white"
+                            >
+                                Some of my projects
+                            </Text>
+                            <Text
+                                size="sm"
+                                className="projects-description text-gray-300"
+                                italic
+                            >
+                                Here are some of my projects that I've worked on
+                                or I'm currently working on
+                            </Text>
+                        </div>
+                        <Link href="/projects">
+                            <Button>All Projects</Button>
+                        </Link>
+                    </div>
+                    <div className="projects-preview">
+                        {projects.slice(0, 2).map((project, index) => (
+                            <ProjectCard key={index} project={project} />
+                        ))}
+                    </div>
+                </motion.div>
+            </main>
+        </div>
+    );
 }
