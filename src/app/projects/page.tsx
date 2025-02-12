5ae7b4eaaf612203655a665c7ab100c93433c7af
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-02-12 16:11:33 +0200

    Started on projects

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import "./style.css";
+
+export default function Projects() {
+    return (
+        <div className="projects-wrapper">
+            <Navbar />
+            <div className="content">
+                <h1>Projects</h1>
+            </div>
+        </div>
+    );
+}
